e94d3a6c7d1fead670680fb8891034d6a96ce511
Emerson Oliveira <emersonoliveira@apoioprodesp.sp.gov.br>
2024-10-11 14:28:24 -0300

    feat: Ajuste rotas.

--- api-gateway-service/api-gateway-service/src/main/java/com/programming/techie/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/api-gateway-service/src/main/java/com/programming/techie/apigatewayservice/ApiGatewayServiceApplication.java
@@ -2,7 +2,10 @@ package com.programming.techie.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,6 +15,14 @@ public class ApiGatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayServiceApplication.class, args);
+    }
+    
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+    	return builder.routes()
+    			.route(r -> r.path("/clientes/***").uri("lb://msclientes"))
+    			.route(r -> r.path("/cartoes/***").uri("lb://mscartoes"))
+    			.build();
     }
 }
 
