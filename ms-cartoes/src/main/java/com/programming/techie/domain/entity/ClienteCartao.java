6e9dd6d6696dd67739b21ce0e99571cb74dbf8be
Emerson Oliveira <emersonoliveira@apoioprodesp.sp.gov.br>
2024-10-14 19:25:22 -0300

    feat: Funcionalidades cartões

--- ms-cartoes/src/main/java/com/programming/techie/domain/entity/ClienteCartao.java
+++ ms-cartoes/src/main/java/com/programming/techie/domain/entity/ClienteCartao.java
@@ -2,6 +2,7 @@ package com.programming.techie.domain.entity;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -21,6 +22,8 @@ public class ClienteCartao {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column
 	private String cpf;
 	
 	
@@ -28,6 +31,7 @@ public class ClienteCartao {
 	@JoinColumn(name = "id_cartao")
 	private Cartao cartao;
 	
+	@Column
 	private BigDecimal limite;
 
 
